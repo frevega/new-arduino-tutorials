355ef35e51114334f4605946a09ff8ebfde11a57
Freddy <elmigueluno@gmail.com>
2023-04-19 16:31:33 -0400

    Lesson 67 homework - Improvements: Added functions for all remote buttons

--- 67-remote_controlled_bright_color_rgb_led_homework/67-remote_controlled_bright_color_rgb_led_homework.ino
+++ 67-remote_controlled_bright_color_rgb_led_homework/67-remote_controlled_bright_color_rgb_led_homework.ino
@@ -42,7 +42,8 @@ Color colors[] = {
   Color(255, 20, 0),    // ORANGE
   Color(255, 50, 150),  // LILAC
   Color(112, 180, 45),  // AQUAMARINE
-  Color(0, 0, 0)        // OFF
+  Color(0, 0, 0),       // OFF
+  Color(255, 255, 255)  // ON - WHITE
 };
 
 enum ColorIndex {
@@ -56,10 +57,11 @@ enum ColorIndex {
   ORANGE,
   LILAC,
   AQUAMARINE,
-  OFF
+  OFF,
+  ON
 };
 
-int redPin = 5, greenPin = 6, bluePin = 9, selectedColorIndex = 0;
+int redPin = 5, greenPin = 6, bluePin = 9, selectedColorIndex = -1;
 float dimmFactor = 1;
 bool isOn = false;
 
@@ -79,16 +81,62 @@ void loop() {
   delay(50);
 }
 
-void setColor(ColorIndex index, bool shouldOverride = false) {
-  if (!isOn && !shouldOverride) { return; }
-  
-  analogWrite(redPin, colors[index].red * dimmFactor);
-  analogWrite(greenPin, colors[index].green * dimmFactor);
-  analogWrite(bluePin, colors[index].blue * dimmFactor);
+void setColor() {
+  printInfo();
+  if (!isOn && selectedColorIndex != OFF) {
+    return;
+  }
+
+  analogWrite(redPin, colors[selectedColorIndex].red * dimmFactor);
+  analogWrite(greenPin, colors[selectedColorIndex].green * dimmFactor);
+  analogWrite(bluePin, colors[selectedColorIndex].blue * dimmFactor);
+}
+
+void selectLight(ColorIndex index) {
+  if (!isOn) {
+    return;
+  }
+
+  if (index -1 == ON) {
+    index = RED;
+  } else if (index > AQUAMARINE) {
+    index = WHITE;
+  } else if (index < WHITE) {
+    index = AQUAMARINE;
+  }
+
+  selectedColorIndex = index;
+}
+
+void colorTest() {
+  if (!isOn) {
+    return;
+  }
+
+  for (int i = 0; i < (sizeof(colors) / sizeof(Color)) - 2; i++) {
+    analogWrite(redPin, colors[i].red);
+    analogWrite(greenPin, colors[i].green);
+    analogWrite(bluePin, colors[i].blue);
+    delay(1000);
+  }
+
+  setColor();
+}
+
+void printInfo() {
+  Serial.print(isOn ? "ON \t" : "OFF \t");
+  Serial.print(selectedColorIndex);
+  Serial.print(" \t");
+  Serial.print(dimmFactor, 5);
+  Serial.print(" \t");
+  Serial.print(colors[selectedColorIndex].red * dimmFactor, 5);
+  Serial.print(" \t");
+  Serial.print(colors[selectedColorIndex].green * dimmFactor, 5);
+  Serial.print(" \t");
+  Serial.println(colors[selectedColorIndex].blue * dimmFactor, 5);
 }
 
 void handleRemoteCommand(IRData data) {
-  bool shouldOverride = false;
   switch (data.command) {
     case ONE:
       selectedColorIndex = RED;
@@ -120,19 +168,30 @@ void handleRemoteCommand(IRData data) {
     case ZERO:
       selectedColorIndex = WHITE;
       break;
+    case ASTERISK:
+      dimmFactor = dimmFactor < 1 ? 1 : 0.01;
+      break;
+    case HASH:
+      return colorTest();
+      break;
     case UP:
-       dimmFactor = (dimmFactor * 1.75) > 1 ? 1 : dimmFactor * 1.75;
+      dimmFactor = (dimmFactor * 1.75) > 1 ? 1 : dimmFactor * 1.75;
       break;
     case DOWN:
-       dimmFactor = (dimmFactor * .75) <= 0.01 ? 0.01 : dimmFactor * .75;
+      dimmFactor = (dimmFactor * .75) <= 0.01 ? 0.01 : dimmFactor * .75;
+      break;
+    case LEFT:
+      selectLight(selectedColorIndex -1);
+      break;
+    case RIGHT:
+      selectLight(selectedColorIndex +1);
       break;
     case OK:
-      selectedColorIndex = isOn ? OFF : WHITE;
-      shouldOverride = true;
+      selectedColorIndex = isOn ? OFF : ON;
       dimmFactor = 1;
       isOn = !isOn;
       break;
   }
 
-  setColor(selectedColorIndex, shouldOverride);
+  setColor();
 }
